976181af9360a2b3f96a3782a7ca2efbfc1e3041
Reiner Gerecke <me@reinergerecke.de>
2018-05-28 06:36:21 +0200

    Format code using black

--- main.py
+++ main.py
@@ -5,23 +5,24 @@ from scapy.utils import hexdump, raw
 
 
 class Tracker:
+
     def __init__(self, debug=False):
         self.debug = debug
         self.socket = bluetooth.BluetoothUserSocket()
         self.connection_handle = None
-    
+
     def _send(self, p):
         self.socket.send(p)
         if self.debug:
-            print('Sending ...')
+            print("Sending ...")
             hexdump(p)
             print()
 
     def connect(self, hwaddr):
         p = (
-            bluetooth.HCI_Hdr() /
-            bluetooth.HCI_Command_Hdr(opcode=0x200d) /
-            bluetooth.HCI_Cmd_LE_Create_Connection(
+            bluetooth.HCI_Hdr()
+            / bluetooth.HCI_Command_Hdr(opcode=0x200d)
+            / bluetooth.HCI_Cmd_LE_Create_Connection(
                 interval=96,
                 window=48,
                 filter=0,
@@ -33,7 +34,9 @@ class Tracker:
                 latency=0,
                 timeout=500,
                 min_ce=0,
-                max_ce=0))
+                max_ce=0,
+            )
+        )
         self._send(p)
         while True:
             p = self.socket.recv()
@@ -43,15 +46,15 @@ class Tracker:
             if p.type == 4:
                 if p.code == 0x3e and p.event == 1 and p.status == 0:
                     if self.debug:
-                        print(f'Connected with handle {p.handle}')
+                        print(f"Connected with handle {p.handle}")
                     self.connection_handle = p.handle
                     return
-    
+
     def disconnect(self):
         p = (
-            bluetooth.HCI_Hdr() /
-            bluetooth.HCI_Command_Hdr(opcode=0x0406) /
-            bluetooth.HCI_Cmd_Disconnect(handle=self.connection_handle)
+            bluetooth.HCI_Hdr()
+            / bluetooth.HCI_Command_Hdr(opcode=0x0406)
+            / bluetooth.HCI_Cmd_Disconnect(handle=self.connection_handle)
         )
         self._send(p)
         while True:
@@ -59,24 +62,25 @@ class Tracker:
             if p.type == 4:
                 if p.code == 0x5:
                     return
-    
+
     def read_attribute(self, gatt_handle):
         p = (
-            bluetooth.HCI_Hdr(type=2) /
-            bluetooth.HCI_ACL_Hdr(handle=self.connection_handle) /
-            bluetooth.L2CAP_Hdr(cid=4) /
-            bluetooth.ATT_Hdr() /
-            bluetooth.ATT_Read_Request(gatt_handle=gatt_handle))
+            bluetooth.HCI_Hdr(type=2)
+            / bluetooth.HCI_ACL_Hdr(handle=self.connection_handle)
+            / bluetooth.L2CAP_Hdr(cid=4)
+            / bluetooth.ATT_Hdr()
+            / bluetooth.ATT_Read_Request(gatt_handle=gatt_handle)
+        )
         self._send(p)
         while True:
             p = self.socket.recv()
             if p.type == 2 and p.opcode == 0xb:
-                return p.value 
-    
+                return p.value
+
 
 try:
     tracker = Tracker()
-    tracker.connect('CD:0B:5E:7C:2E:2A')
+    tracker.connect("CD:0B:5E:7C:2E:2A")
     # manufacturer name
     hexdump(tracker.read_attribute(0x0024))
     # ???
